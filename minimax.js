f83434dd3f3fbff23d09084f2dbfde24e2a423f7
Rick Kitts <rkitts@guidewire.com>
2019-01-22 18:00:35 -0800

    Refactor some common code.

--- minimax.js
+++ minimax.js
@@ -1,6 +1,6 @@
 "use strict"
 
-class MiniMax{
+class MiniMax extends MoveCalculator{
     calculateBestMove(game){
         let newGameMoves = game.ugly_moves();
         let bestEvaluation = -Infinity;
@@ -47,39 +47,4 @@ class MiniMax{
         }
         return(retVal);
     }
-
-    evaluateBoard(board) {
-		let totalEvaluation = 0;
-		for (let i = 0; i < 8; i++) {
-			for (let j = 0; j < 8; j++) {
-				totalEvaluation = totalEvaluation + this.getPieceValue(board[i][j]);
-			}
-		}
-		return totalEvaluation;
-    }
-    
-    getPieceValue(piece) {
-		if (piece === null) {
-			return 0;
-		}
-		let getAbsoluteValue = function (piece) {
-			if (piece.type === 'p') {
-				return 10;
-			} else if (piece.type === 'r') {
-				return 50;
-			} else if (piece.type === 'n') {
-				return 30;
-			} else if (piece.type === 'b') {
-				return 30 ;
-			} else if (piece.type === 'q') {
-				return 90;
-			} else if (piece.type === 'k') {
-				return 900;
-			}
-			throw "Unknown piece type: " + piece.type;
-		};
-	
-		let absoluteValue = getAbsoluteValue(piece, piece.color === 'w');
-		return piece.color === 'w' ? absoluteValue : -absoluteValue;
-	}
 }
--- simple_calc.js
+++ simple_calc.js
@@ -1,6 +1,6 @@
 "use strict"
 
-class SimpleCalculator{
+class SimpleCalculator extends MoveCalculator{
 
     calculateBestMove(game) {
 		let newGameMoves = game.ugly_moves();
@@ -23,40 +23,5 @@ class SimpleCalculator{
 		}
 
 		return bestMove;
-	}
-
-	evaluateBoard(board) {
-		let totalEvaluation = 0;
-		for (let i = 0; i < 8; i++) {
-			for (let j = 0; j < 8; j++) {
-				totalEvaluation = totalEvaluation + this.getPieceValue(board[i][j]);
-			}
-		}
-		return totalEvaluation;
-	}
-
-	getPieceValue(piece) {
-		if (piece === null) {
-			return 0;
-		}
-		let getAbsoluteValue = function (piece) {
-			if (piece.type === 'p') {
-				return 10;
-			} else if (piece.type === 'r') {
-				return 50;
-			} else if (piece.type === 'n') {
-				return 30;
-			} else if (piece.type === 'b') {
-				return 30 ;
-			} else if (piece.type === 'q') {
-				return 90;
-			} else if (piece.type === 'k') {
-				return 900;
-			}
-			throw "Unknown piece type: " + piece.type;
-		};
-	
-		let absoluteValue = getAbsoluteValue(piece, piece.color === 'w');
-		return piece.color === 'w' ? absoluteValue : -absoluteValue;
 	}
 }
